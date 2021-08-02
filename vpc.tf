9a4f9cbd16cb7e42b0f667e9220ec3789538d88e
dinushchathurya <dinushchathurya21@gmail.com>
2021-08-02 21:04:15 +0530

    :sparkles: Script tp create VPC and Subnet

---
+++ vpc.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region  = "eu-west-1"
+    shared_credentials_file = "/Users/admin/.aws/credentials"
+    profile                 = "default"
+}
+
+resource "aws_vpc" "first_vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name="Production"
+    }
+}
+
+resource "aws_subnet" "first_subnet" {
+    vpc_id = aws_vpc.first_vpc.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name="Production-Subnet"
+    }
+}
