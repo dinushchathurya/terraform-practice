025a8eb6562411b38be22d103f893d3f095e44ee
dinushchathurya <dinushchathurya21@gmail.com>
2021-08-02 21:25:19 +0530

    :sparkles: Create Route table

--- main.tf
+++ main.tf
@@ -16,3 +16,22 @@ resource "aws_vpc" "production-vpc" {
 resource "aws_internet_gateway" "gateway" {
     vpc_id = aws_vpc.production-vpc.id
 }
+
+# create Route table
+resource "aws_route_table" "production-route-table" {
+  vpc_id = aws_vpc.production-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "production"
+  }
+}
